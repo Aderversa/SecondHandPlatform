04e520580089e9348e809236dc97f133ff4c2ed0
Aderversa <2457699535@aliyun.com>
2024-04-29 10:20:11 +0800

    package the project by docker

---
+++ package/mysql-ini.sql
@@ -0,0 +1,46 @@
+create database if not exists second_hand;
+
+use second_hand;
+
+create table user
+(
+    id int unsigned primary key auto_increment comment '用户ID',
+    username varchar(20) not null comment '用户名',
+    password varchar(50) not null comment '密码',
+    nickname varchar(20) default '' comment '昵称',
+    email varchar(128) default '' comment '邮箱',
+    user_img varchar(128) default '' null comment '头像',
+    update_time date not null comment '更新时间',
+    create_time date not null comment '创建时间'
+)default character set utf8mb4 comment '用户表';
+
+create table book_type
+(
+    id int unsigned primary key comment '类别ID',
+    name varchar(20) not null comment '类别名'
+)default character set utf8mb4 comment '书籍类别';
+
+create table book
+(
+    id int unsigned primary key auto_increment comment '书籍ID',
+    type_id int unsigned not null comment '类别',
+    price decimal(12, 2) not null comment '价格',
+    isbn varchar(20) not null comment 'ISBN号',
+    img varchar(128) null comment '头像',
+    detail varchar(500) default '' null comment '书籍描述',
+    release_time date not null comment '上架时间',
+    seller_id int unsigned not null comment '卖家ID',
+    purchased int not null comment '是否已被购买',
+    constraint type foreign key (type_id) references book_type(id),
+    constraint seller foreign key (seller_id) references user(id)
+);
+
+create table `order`
+(
+    user_id int unsigned not null comment '用户ID',
+    book_id int unsigned not null comment '书籍ID',
+    pay_time date not null comment '下单时间',
+    address varchar(100) not null comment '收货地址',
+    constraint buyer foreign key (user_id) references user(id),
+    constraint want_book foreign key (book_id) references book(id)
+)comment '用户购买的商品';
